215c94163d35f88434f9d0e689ee063b6ecdec7d
Vineet2408 <vineetd84@gmail.com>
2021-02-02 20:09:04 +0530

    movie-cruiserJPA

---
+++ moviecuriser/src/main/java/com/cognizant/moviecuriser/MoviecuriserApplication.java
@@ -0,0 +1,23 @@
+package com.cognizant.moviecuriser;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 
+ * @author Ayush Srivastava
+ *
+ */
+@SpringBootApplication
+@ComponentScan("com.cognizant.moviecuriser.controller","com.cognizant.moviecuriser.model","com.cognizant.moviecuriser.repository","com.cognizant.moviecuriser.service")
+public class MoviecuriserApplication {
+private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(MoviecuriserApplication.class);
+	public static void main(String[] args) {
+		LOGGER.info("Start");
+		SpringApplication.run(MoviecuriserApplication.class, args);
+		LOGGER.info("End");
+	}
+
+}
---
+++ moviecuriser/src/main/java/com/cognizant/moviecuriser/service/exception/MovieNotFoundException.java
@@ -0,0 +1,14 @@
+package com.cognizant.moviecuriser.service.exception;
+/**
+ * 
+ * @author Ayush Srivastava
+ *
+ */
+public class MovieNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
